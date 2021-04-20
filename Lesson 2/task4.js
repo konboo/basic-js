78b5b10713ef0f0a950231a9f74abc4dd56d4893
Constantine <kon.bober@yandex.ru>
2021-04-20 11:01:55 +0200

    Home Work for lesson 2

---
+++ Lesson 2/task4.js
@@ -0,0 +1,40 @@
+"use strict";
+/**
+ * Функция сложения двух параметров
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function sum(a, b) {
+  return a + b;
+}
+
+/**
+ * Функция умножения двух параметров
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function mul(a, b) {
+  return a * b;
+}
+
+/**
+ * Функция деления параметров
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function div(a, b) {
+  return a / b;
+}
+
+/**
+ * Функция вычитания двух параметров
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+function ded(a, b) {
+  return a - b;
+}
